8bdfead515e4e6dbaa85578de212aaa066a0aa3c
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-05-18 18:29:48 +0600

    added private route and active navlink

--- src/components/page/shared/NavigationBar/NavigationBar.jsx
+++ src/components/page/shared/NavigationBar/NavigationBar.jsx
@@ -1,21 +1,51 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import ActiveLink from "../ActiveLink/ActiveLink";
+import { useContext } from "react";
+import { AuthContext } from "../../../../providers/AuthProvider";
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
+    const {user, logOut} = useContext(AuthContext);
+    console.log(user);
+    const handleLogout = ()=>{
+        logOut()
+        .then(()=>{
+
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
     return (
         <div>
-            <Navbar bg="primary" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Car Craze</Navbar.Brand>
-                    <Nav className="mx-auto">
-                        <Link to="/" className="text-white me-2 text-decoration-none">Home</Link>
-                        <Link to="/alltoy" className="text-white me-2 text-decoration-none">All-Toy</Link>
-                        <Link to="/blog" className="text-white me-2 text-decoration-none">Blogs</Link>
-                        <Link to="/login" className="text-white me-2 text-decoration-none">Login</Link>
-                    </Nav>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="mx-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <ActiveLink to='/'>Home</ActiveLink>
+                            <ActiveLink to='/alltoy'>All-Toy</ActiveLink>
+                            <ActiveLink to='/blog'>Blog</ActiveLink>
+                            
+                        </Nav>
+                        {
+                            user 
+                            ?<img src={user?.photoURL} alt="" style={{ width: '35px' }} className="rounded-circle mx-3" />
+                            :<span>{user?.email}</span>
+                        }
+                        {
+                            user
+                            ? <Button variant="outline-danger" onClick={handleLogout}>Logout</Button>
+                            : <Link to='/login'><Button variant="outline-success">Login</Button></Link>
+                        }
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
-
         </div>
     );
 };
